edc371be6791c795396854b0ad0d8da8453c6151
TamerDarwish <darwishtamer99@gmail.com>
2023-02-22 16:12:45 +0200

    add functionality to the buttons

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+$("#load-user-button").on("click",function(){
+   let apiObject = new APIManager()
+
+   $("#display-button").on("click",function(){
+    apiObject.loadData()
+   })
+  
+  })
+
+  
+ 
+
 let api = new APIManager()
 //let render = new Renderer()
-let myData = api.loadData()
-console.log(api.data)
+//let myData = api.loadData()
+//console.log(api.data)
 //render.userRender(api.data)
